a757234ac15c580c4e5143ff4ff3092562cd0ec2
Anton Shvab <shwab.ay@gmail.com>
2017-03-15 14:42:55 +0000

    correction to rules

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import readlineSync from 'readline-sync';
 export const sayHello = () => {
   const name = readlineSync.question('May I have your name, human being? ');
   console.log(`Hello, dear ${name}!`);
-  return;
-}
+};
