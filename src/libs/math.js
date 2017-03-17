d4db8b79dc2c5250bc61b163730ec913382cc7cb
Anton Shvab <shwab.ay@gmail.com>
2017-03-17 01:27:06 +0000

    add gcd

--- src/libs/math.js
+++ src/libs/math.js
@@ -1,8 +1,13 @@
-const generateRandomIntegers = (count, min, max) => {
+export const generateRandomIntegers = (count, min, max) => {
   const getRandomInt = () => Math.floor(Math.random() * (max - min)) + min;
   const randomIntegers = Array.from({ length: count }, getRandomInt);
   return randomIntegers;
 };
 
-// export { generateRandomIntegers };
-export default generateRandomIntegers;
+export const gcd = (numA, numB) => {
+  if (numB === 0) {
+    return numA;
+  }
+  return gcd(numB, numA % numB);
+};
+
