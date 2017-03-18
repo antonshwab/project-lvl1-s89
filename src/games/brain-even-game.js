c486a600ed2490b63a3c5f0c5508f00c8aa11da3
Anton Shvab <shwab.ay@gmail.com>
2017-03-18 15:54:57 +0000

    refactoring

--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -1,27 +1,24 @@
-import { generateRandomIntegers, isEven } from '../libs/math';
+import { getRandomInt, isEven } from '../libs/math';
 
-const makeIntegerRiddles = (riddlesCount) => {
-  const integersCount = riddlesCount;
-  const minInt = 0;
-  const maxInt = 100;
-  return generateRandomIntegers(integersCount, minInt, maxInt);
+const getAnswer = (int) => {
+  if (isEven(int)) {
+    return 'yes';
+  }
+  return 'no';
 };
 
-const prepareRiddlesWithAnswers = (riddlesCount) => {
-  const riddles = makeIntegerRiddles(riddlesCount);
-  const riddlesWithAnswers = riddles.map((int) => {
-    if (isEven(int)) {
-      return [int, 'yes'];
-    }
-    return [int, 'no'];
-  });
+const prepareRiddlesWithAnswers = () => {
+  const minInt = 0;
+  const maxInt = 100;
+  const riddles =
+    [getRandomInt(minInt, maxInt), getRandomInt(minInt, maxInt), getRandomInt(minInt, maxInt)];
+  const riddlesWithAnswers = riddles.map(riddle => [riddle, getAnswer(riddle)]);
   return riddlesWithAnswers;
 };
 
 const rules = 'Answer "yes" if number even otherwise answer "no".\n';
 
-const riddlesCount = 3;
-const riddlesWithAnswers = prepareRiddlesWithAnswers(riddlesCount);
+const riddlesWithAnswers = prepareRiddlesWithAnswers();
 
 const brainEvenGame = {
   rules,
