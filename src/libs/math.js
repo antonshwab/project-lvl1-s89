1aeb8314a5f4a7c477e30a4cd0238f96422e6cfe
Anton Shvab <shwab.ay@gmail.com>
2017-03-18 10:42:28 +0000

    add functions

--- src/libs/math.js
+++ src/libs/math.js
@@ -14,3 +14,20 @@ export const gcd = (numA, numB) => {
 };
 
 export const isEven = i => i % 2 === 0;
+
+export const digitsToString = digits => digits.join('');
+
+export const intToDigits = (int) => {
+  const iter = (num, digits) => {
+    if (num < 10) {
+      return [num, ...digits];
+    }
+    const lessNum = Math.floor(num / 10);
+    const digit = num % 10;
+    return iter(lessNum, [digit, ...digits]);
+  };
+  return iter(int, []);
+};
+
+export const ascNumSort = nums => nums.sort((a, b) => a - b);
+
